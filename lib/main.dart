07fcdd0b004aa5326f8b6c35534b60992872502f
Leon <leon.lukas11@web.de>
2018-04-20 12:04:01 +0200

    proto

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,17 @@ class ExpansionTileSample extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('FK07'),
-        ),
+        appBar: new AppBar(title: const Text('FK07'),actions: <Widget>[
+          new Image.asset(
+            'images/logo.bmp',
+            width: 75.0,
+            height: 75.0,
+            fit: BoxFit.cover,
+          ),
+        ]),
         body: new ListView.builder(
-          itemBuilder: (BuildContext context, int index) => new EntryItem(data[index]),
+          itemBuilder: (BuildContext context, int index) =>
+          new EntryItem(data[index]),
           itemCount: data.length,
         ),
       ),
@@ -19,46 +25,89 @@ class ExpansionTileSample extends StatelessWidget {
 
 // One entry in the multilevel list displayed by this app.
 class Entry {
-  Entry(this.title, [this.children = const [], this.info = const <String,String>{}]);
+  Entry(this.title,
+      [this.children = const [], this.info = const <String, String>{}]);
+
   final String title;
   List<Entry> children;
-  final Map<String,String> info;
+  final Map<String, String> info;
 }
 
 // The entire multilevel list displayed by this app.
 final List<Entry> data = <Entry>[
-  new Entry('Tecnical Writing',
+  new Entry(
+    'Tecnical Writing',
     <Entry>[
-      new Entry('Info',[],<String,String>{"ECTS":"5","SWS":"4","lecturer":"Dr. phil. Aniko Balazs", }),
-      new Entry('Details',
-          <Entry>[
-            new Entry('Info',[],<String,String>{"Course objective":"Enable participants to produce technical documents according to the"+
-            "English language writing standards. > Sensitise students for the need of document usability and readability."+
-            "> Provide students with the tools necessary to write and revise their documents effectively",
-              "Prerequisites":"Profound knowledge in English (intermediate -- post-intermediate)","Recommended reading":
-              "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003."
-            + "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990."+
-              "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989", }),
-          ],),
-      new Entry('Section A2'),
-    ],
-  ),
-  new Entry('Chapter B',
-    <Entry>[
-      new Entry('Section B0'),
-      new Entry('Section B1'),
-    ],
-  ),
-  new Entry('Chapter C',
-    <Entry>[
-      new Entry('Section C0'),
-      new Entry('Section C1'),
-      new Entry('Section C2',
+      new Entry('Info', [], <String, String>{
+        "ECTS": "5",
+        "SWS": "4",
+        "lecturer": "Dr. phil. Aniko Balazs",
+      }),
+      new Entry(
+        'Details',
         <Entry>[
-          new Entry('Item C2.0'),
-          new Entry('Item C2.1'),
-          new Entry('Item C2.2'),
-          new Entry('Item C2.3'),
+          new Entry('Info', [], <String, String>{
+            "Course objective":
+            "Enable participants to produce technical documents according to the" +
+                "English language writing standards. > Sensitise students for the need of document usability and readability." +
+                "> Provide students with the tools necessary to write and revise their documents effectively",
+            "Prerequisites":
+            "Profound knowledge in English (intermediate -- post-intermediate)",
+            "Recommended reading":
+            "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
+                "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
+                "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
+          }),
+        ],
+      ),
+    ],
+  ),
+  new Entry(
+    'Operating Systems 1',
+    <Entry>[
+      new Entry('Info', [], <String, String>{
+        "ECTS": "5",
+        "SWS": "4",
+        "lecturer": "Dr. phil. Aniko Balazs",
+      }),
+      new Entry(
+        'Details',
+        <Entry>[
+          new Entry('Info', [], <String, String>{
+            "Course objective":
+            "Enable participants to produce technical documents according to the" +
+                "English language writing standards. > Sensitise students for the need of document usability and readability." +
+                "> Provide students with the tools necessary to write and revise their documents effectively",
+            "Prerequisites":
+            "Profound knowledge in English (intermediate -- post-intermediate)",
+            "Recommended reading":
+            "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
+                "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
+                "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
+          }),
+        ],
+      ),
+    ],
+  ),
+  new Entry(
+    'Event Driven Systems',
+    <Entry>[
+      new Entry('Info', [], <String, String>{
+        "ECTS": "5",
+        "SWS": "4",
+        "lecturer": "Dr. phil. Aniko Balazs",
+      }),
+      new Entry(
+        'Details',
+        <Entry>[
+          new Entry('Info', [], <String, String>{
+            "Sed cursus": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus mauris quis diam porttitor varius." +
+                " Cras egestas, quam non tempor commodo, metus nisi elementum leo, et ultrices enim leo pulvinar justo." +
+                " Cras condimentum rhoncus lacinia. Sed ultrices efficitur ipsum, ut imperdiet tortor bibendum eget. " +
+                "Curabitur nec luctus libero. Donec eu ante mi. Suspendisse diam tortor, fringilla non vestibulum eu, " +
+                "tristique eu quam. Maecenas consequat convallis velit id suscipit. Proin blandit imperdiet dolor, ut" +
+                " varius orci faucibus at. Sed quis cursus eros. Proin eu neque massa. ",
+          }),
         ],
       ),
     ],
@@ -73,14 +122,26 @@ class EntryItem extends StatelessWidget {
   final Entry entry;
 
   Widget _buildTiles(Entry root) {
-    if (root.children.isEmpty&&root.info==null)
+    if (root.children.isEmpty && root.info == null)
       return new ListTile(title: new Text(root.title));
-    if (root.children.isEmpty&&root.info.isNotEmpty){
+    if (root.children.isEmpty && root.info.isNotEmpty) {
       Iterable<String> keys = root.info.keys;
       List<TableRow> rows = new List();
-      keys.forEach((x) => rows.add(new TableRow(children: [new Text(x),new Text(root.info[x])])));
+      for (String s in keys) {
+        rows.add(new TableRow(children: [
+          new Text(s,
+              style: new TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold))
+        ]));
+        rows.add(new TableRow(children: [
+          new Text(root.info[s], style: new TextStyle(fontSize: 14.0))
+        ]));
+      }
+//      keys.forEach((x) => rows.add(new TableRow(children: [new Text(x),new Text(root.info[sx])])));
+      //keys.forEach((x) => rows.add(new TableRow(children: [new Text(x)])));
+      //keys.forEach((x) => rows.add(new TableRow(children: [new Text(root.info[x])])));
       return new Table(
         children: rows,
+        border: new TableBorder.all(),
       );
     }
     return new ExpansionTile(
@@ -89,6 +150,7 @@ class EntryItem extends StatelessWidget {
       children: root.children.map(_buildTiles).toList(),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return _buildTiles(entry);
