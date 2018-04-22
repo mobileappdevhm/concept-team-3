fa8c8457e8ef102e7ac049c49343a018a33c5f1a
Leon <leon-lukas11@web.de>
2018-04-22 16:56:53 +0200

    like button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class ExpansionTileSample extends StatelessWidget {
+class ExpandingFaculty extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(title: const Text('FK07'),actions: <Widget>[
+        appBar: new AppBar(title: const Text('FK07'), actions: <Widget>[
           new Image.asset(
             'images/logo.bmp',
             width: 75.0,
@@ -13,96 +13,100 @@ class ExpansionTileSample extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ]),
-        body:new Padding(
-    padding: new EdgeInsets.all(5.0),
-    child: new ListView.builder(
-          itemBuilder: (BuildContext context, int index) =>
-          new EntryItem(data[index]),
-          itemCount: data.length,
+        body: new Padding(
+          padding: new EdgeInsets.all(5.0),
+          child: new ListView.builder(
+            itemBuilder: (BuildContext context, int index) =>
+                new EntryItem(data[index]),
+            itemCount: data.length,
+          ),
         ),
-        ),),
+      ),
     );
   }
 }
 
 // One entry in the multilevel list displayed by this app.
-class Entry {
-  Entry(this.title,
+class Entry{
+  Entry(this.title, this.likeable,
       [this.children = const [], this.info = const <String, String>{}]);
 
   final String title;
   List<Entry> children;
   final Map<String, String> info;
+  final bool likeable;
+  bool liked = false;
+
 }
 
 // The entire multilevel list displayed by this app.
 final List<Entry> data = <Entry>[
   new Entry(
-    'Tecnical Writing',
+    'Tecnical Writing', true,
     <Entry>[
-      new Entry('Info', [], <String, String>{
+      new Entry('Info',false, [], <String, String>{
         "ECTS": "5",
         "SWS": "4",
         "lecturer": "Dr. phil. Aniko Balazs",
       }),
       new Entry(
-        'Details',
+        'Details',false,
         <Entry>[
-          new Entry('Info', [], <String, String>{
+          new Entry('Info',false, [], <String, String>{
             "Course objective":
-            "Enable participants to produce technical documents according to the" +
-                "English language writing standards. > Sensitise students for the need of document usability and readability." +
-                "> Provide students with the tools necessary to write and revise their documents effectively",
+                "Enable participants to produce technical documents according to the" +
+                    "English language writing standards. > Sensitise students for the need of document usability and readability." +
+                    "> Provide students with the tools necessary to write and revise their documents effectively",
             "Prerequisites":
-            "Profound knowledge in English (intermediate -- post-intermediate)",
+                "Profound knowledge in English (intermediate -- post-intermediate)",
             "Recommended reading":
-            "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
-                "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
-                "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
+                "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
+                    "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
+                    "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
           }),
         ],
       ),
     ],
   ),
   new Entry(
-    'Operating Systems 1',
+    'Operating Systems 1',true,
     <Entry>[
-      new Entry('Info', [], <String, String>{
+      new Entry('Info',false, [], <String, String>{
         "ECTS": "5",
         "SWS": "4",
         "lecturer": "Dr. phil. Aniko Balazs",
       }),
       new Entry(
-        'Details',
+        'Details',false,
         <Entry>[
-          new Entry('Info', [], <String, String>{
+          new Entry('Info', false,[], <String, String>{
             "Course objective":
-            "Enable participants to produce technical documents according to the" +
-                "English language writing standards. > Sensitise students for the need of document usability and readability." +
-                "> Provide students with the tools necessary to write and revise their documents effectively",
+                "Enable participants to produce technical documents according to the" +
+                    "English language writing standards. > Sensitise students for the need of document usability and readability." +
+                    "> Provide students with the tools necessary to write and revise their documents effectively",
             "Prerequisites":
-            "Profound knowledge in English (intermediate -- post-intermediate)",
+                "Profound knowledge in English (intermediate -- post-intermediate)",
             "Recommended reading":
-            "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
-                "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
-                "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
+                "> Rechenberg, P., \"Technisches Schreiben (nicht nur) fuer Informatiker\", 2nd ed., Muenchen-Wien: Hanser, 2003." +
+                    "> Gopen, G. D. and Swan, J. A., \"The Science of Scientific Writing,\" American Scientist, vol. 78, pp. 550-558, Nov.1990." +
+                    "> Knuth, D.E. et al., \"Mathematical Writing\", MAA Notes, no. 14, The Mathematical Association of America, 1989",
           }),
         ],
       ),
     ],
   ),
   new Entry(
-    'Event Driven Systems',
+    'Event Driven Systems',true,
     <Entry>[
-      new Entry('Info', [], <String, String>{
+      new Entry('Info', false,[], <String, String>{
         "ECTS": "5",
         "SWS": "4",
         "lecturer": "Dr. phil. Aniko Balazs",
       }),
       new Entry(
-        'Details',
+        'Details',true,
         <Entry>[
-          new Entry('Info', [], <String, String>{
+          new Entry('Info',false, [], <String, String>{
             "Sed cursus": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus mauris quis diam porttitor varius." +
                 " Cras egestas, quam non tempor commodo, metus nisi elementum leo, et ultrices enim leo pulvinar justo." +
                 " Cras condimentum rhoncus lacinia. Sed ultrices efficitur ipsum, ut imperdiet tortor bibendum eget. " +
@@ -131,21 +135,19 @@ class EntryItem extends StatelessWidget {
       List<TableRow> rows = new List();
       for (String s in keys) {
         rows.add(new TableRow(children: [
-        new Padding(
-        padding: new EdgeInsets.all(8.0),
-            child:new Text(s,
-              style: new TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold))
-        )
+          new Padding(
+              padding: new EdgeInsets.all(8.0),
+              child: new Text(s,
+                  style: new TextStyle(
+                      fontSize: 17.0, fontWeight: FontWeight.bold)))
         ]));
         rows.add(new TableRow(children: [
-        new Padding(
-        padding: new EdgeInsets.all(4.0),
-            child: new Text(root.info[s], style: new TextStyle(fontSize: 14.0)))
+          new Padding(
+              padding: new EdgeInsets.all(4.0),
+              child:
+                  new Text(root.info[s], style: new TextStyle(fontSize: 14.0)))
         ]));
       }
-//      keys.forEach((x) => rows.add(new TableRow(children: [new Text(x),new Text(root.info[sx])])));
-      //keys.forEach((x) => rows.add(new TableRow(children: [new Text(x)])));
-      //keys.forEach((x) => rows.add(new TableRow(children: [new Text(root.info[x])])));
       return new Table(
         children: rows,
         border: new TableBorder.all(),
@@ -155,15 +157,58 @@ class EntryItem extends StatelessWidget {
       key: new PageStorageKey<Entry>(root),
       title: new Text(root.title),
       children: root.children.map(_buildTiles).toList(),
+      trailing:   root.likeable ? new MaterialButton(onPressed: null,
+       child: new Icon(
+       root.liked ? Icons.favorite : Icons.favorite_border,
+       color: root.liked ? Colors.red : null,)):null ,
+//      trailing: root.likeable
+//          ?  new MaterialButton(onPressed: null,
+//       child: new Icon(
+//        root.liked ? Icons.favorite : Icons.favorite_border,
+//        color: root.liked ? Colors.red : null,)) : null,
+
+
     );
   }
 
-  @override
+
   Widget build(BuildContext context) {
     return _buildTiles(entry);
   }
+
 }
+//class entryState extends State<Entry> {
+//  bool _isFavorited = false;
+//
+//  void _toggleFavorite() {
+//    setState(() {
+//      // If the lake is currently favorited, unfavorite it.
+//      if (_isFavorited) {
+//        _isFavorited = false;
+//        // Otherwise, favorite it.
+//      } else {
+//        _isFavorited = true;
+//      }
+//    });
+//    @override
+//    Widget build(BuildContext context) {
+//
+//       return new Entry(title, likeable)
+//  }
+//
+//class Configuration extends InheritedWidget {
+//  final String myConf;
+//
+//  const Configuration({this.myConf, Widget child}): super(child: child);
+//
+//  @override
+//  bool updateShouldNotify(Configuration oldWidget) {
+//    return myConf != oldWidget.myConf;
+//  }
+//}
+
 
 void main() {
-  runApp(new ExpansionTileSample());
+  runApp(new ExpandingFaculty());
 }
+
